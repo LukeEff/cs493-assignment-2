55734e568166ffbbb6be72e2920a05bb4643d4e8
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-05-06 13:51:12 -0700

    replace old user filter with new function calls

--- api/users.js
+++ api/users.js
@@ -22,7 +22,7 @@ router.get('/:userid/businesses', function (req, res) {
  */
 router.get('/:userid/reviews', function (req, res) {
   const userid = parseInt(req.params.userid);
-  const userReviews = reviews.filter(review => review && review.userid === userid);
+  const userReviews = getReviewsByUserID(userid);
   res.status(200).json({
     reviews: userReviews
   });
@@ -33,7 +33,7 @@ router.get('/:userid/reviews', function (req, res) {
  */
 router.get('/:userid/photos', function (req, res) {
   const userid = parseInt(req.params.userid);
-  const userPhotos = photos.filter(photo => photo && photo.userid === userid);
+  const userPhotos = getPhotosByUserID(userid);
   res.status(200).json({
     photos: userPhotos
   });
